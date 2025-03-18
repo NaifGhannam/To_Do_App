acebfe67c59940fdf21b5b592c66f50482ab10c0
Naif Ghannam <dhdfaui@gmail.com>
2025-03-18 09:15:56 +0300

    Initial Commit

---
+++ week4proj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  week4proj
+//
+//  Created by Mac on 18/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ week4proj/week4projApp.swift
@@ -0,0 +1,17 @@
+//
+//  week4projApp.swift
+//  week4proj
+//
+//  Created by Mac on 18/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct week4projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
